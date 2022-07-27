f81084c42f1ba7a51d388a045c618cbc1e20e41c
Kirill Chizhov <sstive39@gmail.com>
2022-07-27 05:33:16 +0300

    Removed packages

--- Assets/Scripts/Camera/CameraConrioller.cs
+++ Assets/Scripts/Camera/CameraConrioller.cs
@@ -1,6 +1,26 @@
-﻿// <copyright file="CameraConrioller.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2022 Kirill Chizhov
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Assets.Scripts.Camera
 {
@@ -9,11 +29,16 @@ namespace Assets.Scripts.Camera
     public class CameraConrioller : MonoBehaviour
     {
         [Header("References")]
-        public Camera MainCamera;
-        public Transform CamHolder;
-        public SpriteRenderer Map;
+
+        [SerializeField]
+        private Camera mainCamera;
+        [SerializeField]
+        private Transform camHolder;
+        [SerializeField]
+        private SpriteRenderer map;
 
         [Header("Zoom")]
+
         [SerializeField]
         private float touchZoomSensivity = 0.01f;
 
@@ -34,7 +59,7 @@ namespace Assets.Scripts.Camera
             // Updating start touch position
             if (Input.GetMouseButtonDown(0))
             {
-                this.dragOrigin = this.MainCamera.ScreenToWorldPoint(Input.mousePosition);
+                this.dragOrigin = this.mainCamera.ScreenToWorldPoint(Input.mousePosition);
             }
 
             // Handling camera drag direction based on start touch position
@@ -52,26 +77,26 @@ namespace Assets.Scripts.Camera
         /* Drag */
         private void Drag()
         {
-            Vector3 direction = this.dragOrigin - this.MainCamera.ScreenToWorldPoint(Input.mousePosition);
-            this.CamHolder.position += direction;
+            Vector3 direction = this.dragOrigin - this.mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            this.camHolder.position += direction;
 
             this.UpdateBounds();
         }
 
         private void UpdateBounds()
         {
-            Vector3 pos = this.CamHolder.position;
+            Vector3 pos = this.camHolder.position;
 
             // Camera
-            Vector3 camSize = this.MainCamera.ViewportToWorldPoint(new Vector3(1, 1, this.MainCamera.nearClipPlane)) - this.CamHolder.position;
+            Vector3 camSize = this.mainCamera.ViewportToWorldPoint(new Vector3(1, 1, this.mainCamera.nearClipPlane)) - this.camHolder.position;
 
-            Vector2 minBounds = this.Map.bounds.min + camSize;
-            Vector2 maxBounds = this.Map.bounds.max - camSize;
+            Vector2 minBounds = this.map.bounds.min + camSize;
+            Vector2 maxBounds = this.map.bounds.max - camSize;
 
             pos.x = Mathf.Clamp(pos.x, minBounds.x, maxBounds.x);
             pos.y = Mathf.Clamp(pos.y, minBounds.y, maxBounds.y);
 
-            this.CamHolder.position = pos;
+            this.camHolder.position = pos;
         }
 
         /* Zoom */
@@ -104,13 +129,13 @@ namespace Assets.Scripts.Camera
         // Zoom in or out
         private void ChangeZoom(float zoomDelta)
         {
-            this.SetZoom(this.MainCamera.orthographicSize - zoomDelta);
+            this.SetZoom(this.mainCamera.orthographicSize - zoomDelta);
         }
 
         // Set exact zoom value
         private void SetZoom(float value)
         {
-            this.MainCamera.orthographicSize = Mathf.Clamp(value, this.zoomMin, this.zoomMax);
+            this.mainCamera.orthographicSize = Mathf.Clamp(value, this.zoomMin, this.zoomMax);
         }
     }
 }
